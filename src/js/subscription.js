455ace4cf7e883cb632bf6081f483fd98ad52351
Matilde Park <matilde@tlon.io>
2019-10-31 20:36:40 -0400

    peering on /primary

--- src/js/subscription.js
+++ src/js/subscription.js
@@ -14,7 +14,7 @@ export class Subscription {
   }
 
   initializesoto() {
-    api.bind('/sole', 'PUT', api.authTokens.ship, 'soto',
+    api.bind('/primary', 'PUT', api.authTokens.ship, 'soto',
       this.handleEvent.bind(this),
       this.handleError.bind(this));
   }
@@ -25,7 +25,7 @@ export class Subscription {
 
   handleError(err) {
     console.error(err);
-    api.bind('/sole', 'PUT', api.authTokens.ship, 'soto',
+    api.bind('/primary', 'PUT', api.authTokens.ship, 'soto',
       this.handleEvent.bind(this),
       this.handleError.bind(this));
   }

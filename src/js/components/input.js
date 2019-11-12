04b8685d68759b565aada30e27309bf0d68cf02e
Matilde Park <matilde@park.computer>
2019-11-11 23:45:18 -0500

    sole-actions sent on keypress, first attempt

--- src/js/components/input.js
+++ src/js/components/input.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { store, buffer } from '../store'
 
 export class Input extends Component {
     constructor(props) {
@@ -8,15 +9,37 @@ export class Input extends Component {
 
     keyPress(e) {
         if (e.keyCode === 13) {
-          console.log("keysent")
+          console.log(e.key)
           api.soto("ret")
         }
+        else if (e.key === "Backspace") {
+          // i'll come to you
+          e.preventDefault();
+        }
+        else if (!(e.key === "Meta" || e.key === "Alt" || e.key === "Control" || e.key === "Escape" || e.key === "Shift")) {
+          let thisact = buffer.transmit({ins:{at: store.state.cursor, cha: e.key}});
+          api.soto({det: thisact});
+          }
       }
 
     render() {
         return (
-          <div className="flex flex-row" style={{ flexGrow: 1 }}>~{this.props.ship}:dojo<span id="prompt">{this.props.prompt}</span>
-              <input autoCorrect="false" autoFocus={true} className="mono ml1 flex-auto dib w-100" onKeyDown={this.keyPress}></input>                
+          <div className="flex flex-row" 
+          style={{ flexGrow: 1 }}>
+            ~{this.props.ship}:dojo
+          <span id="prompt">
+            {this.props.prompt}
+          </span>
+              <input 
+              autoCorrect="false" 
+              autoFocus={true}
+              className="mono ml1 flex-auto dib w-100"
+              onChange={event => {
+                store.setState({cursor: event.target.selectionStart})
+                buffer.buf = event.target.value
+              }}
+              onKeyDown={this.keyPress}>
+              </input>                
             </div>
         )
     }
--- src/js/store.js
+++ src/js/store.js
@@ -6,22 +6,32 @@ export class Store {
     constructor() {
         this.state = {
             txt: [],
-            prompt: ''
+            prompt: '',
+            cursor: 0
         }
-        console.log(buffer);
     }
 
     handleEvent(data) {
+        // this needs a map but also why does dojo say nothing when sending it a sole-action
         let dojoReply = data.data;
         console.log(dojoReply);
-        // pro is our prompt, either > or < depending on the statement
+        // pro is our prompt, either '>' or '<' depending on the statement
         if (dojoReply.pro) this.setState({ prompt: dojoReply.pro.cad});
+        if (dojoReply.tan) {
+            let textLog = this.state.txt;
+            textLog.push(dojoReply.tan);
+            this.setState({ txt: textLog });
+        }
         // responses come in "reply + change to buffer" arrays
         if (dojoReply.constructor === Array) {
+            // txt prints to console history
             if (dojoReply[0].txt) {
                 let textLog = this.state.txt;
                 textLog.push(dojoReply[0].txt);
                 this.setState({ txt: textLog });
+            }
+            else if (dojoReply[0].det) {
+                buffer.receive(dojoReply[0].det);
             }
         }
     }

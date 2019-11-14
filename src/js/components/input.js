8073d820f69b15f5bfe8a9a6bc132c9951011f0f
Matilde Park <matilde@park.computer>
2019-11-13 23:19:50 -0500

    input capture

--- src/js/components/input.js
+++ src/js/components/input.js
@@ -1,80 +1,79 @@
 import React, { Component } from 'react'
-import { store, buffer } from '../store'
+import { store } from '../store'
 import { api } from '../api';
 
 export class Input extends Component {
-    constructor(props) {
-        super(props);
-        this.keyPress = this.keyPress.bind(this);
+  constructor(props) {
+    super(props);
+    this.keyPress = this.keyPress.bind(this);
+    this.inputRef = React.createRef();
+  }
+
+  componentDidUpdate() {
+      this.inputRef.current.setSelectionRange(this.props.cursor, this.props.cursor);
     }
+  
+  keyPress = (e) => {
+    e.preventDefault();
+    let ignoredKeys = ["Meta", "Alt", "Control", "Escape", "Shift",
+    "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8",
+    "F9", "F10", "F11", "F12"
+  ]
+  // submit on enter
+  if (e.keyCode === 13 || e.key === "Enter") {
+    api.soto("ret");
+  }
 
-    keyPress = (e) => {
-      let ignoredKeys = ["Meta", "Alt", "Control", "Escape", "Shift",
-                        "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8",
-                        "F9", "F10", "F11", "F12"
-                        ]
-      // submit on enter
-        if (e.keyCode === 13 || e.key === "Enter") {
-          api.soto("ret")
-        }
-        // remove and resubmit console input if anything is inputted
-          // XX backspace behaviour is fucking weird, look into this
-        else if (e.key === "Backspace") {
-          if (this.props.cursor > 0) {
-            return store.doEdit({ del: this.props.cursor - 1 });
-          } else {
-            e.preventDefault();
-          }
-        }
-        else if (e.key.startsWith("Arrow")) {
-          if (e.key === "ArrowLeft") {
-            (this.props.cursor > 0) ? 
-            store.setState({cursor: this.props.cursor - 1}) : 
-            e.preventDefault();
-          }
-          else if (e.key === "ArrowRight") {
-            (this.props.cursor <= this.props.input.length) ?
-            store.setState({ cursor: this.props.cursor + 1}) :
-            e.preventDefault();
-          }
-          else e.preventDefault(); // XX up/down should browse command history
-        }
-        // tab completion
-        else if (e.key === "Tab") {
-          e.preventDefault();
-          api.soto({tab: this.props.cursor});
-        }
-        // capture and transmit most characters
-        else if (ignoredKeys.indexOf(e.key) === -1) {
-          let thisact = buffer.transmit({ins:{at: this.props.cursor, cha: e.key}});
-          api.soto({det: thisact});
-          }
-      }
-
-      handleChange = (e) => {
-        store.setState({ input: event.target.value });
-        store.setState({ cursor: event.target.selectionEnd });
-      }
-
-    render() {
-        return (
-          <div className="flex flex-row" 
-          style={{ flexGrow: 1 }}>
-            ~{this.props.ship}:dojo
-          <span id="prompt">
-            {this.props.prompt}
-          </span>
-              <input 
-              autoCorrect="false" 
-              autoFocus={true}
-              className="mono ml1 flex-auto dib w-100"
-              onChange={this.handleChange}
-              onKeyDown={this.keyPress}
-              onPaste={e => {e.preventDefault()}}
-              value={this.props.input}/>
-            </div>
-        )
+  else if (e.key === "Backspace") {
+    if (this.props.cursor > 0) {
+      store.doEdit({ del: this.props.cursor - 1});
+      return store.setState({ cursor: this.props.cursor - 1});
     }
+  }
+  else if (e.key.startsWith("Arrow")) {
+    if (e.key === "ArrowLeft") {
+      if (this.props.cursor > 0) {
+        store.setState({ cursor: this.props.cursor - 1 });
+      }
+    }
+    else if (e.key === "ArrowRight") {
+      if (this.props.cursor < this.props.input.length) {
+        store.setState({ cursor: this.props.cursor + 1});
+      }
+    }
+  }
+  // tab completion
+  else if (e.key === "Tab") {
+    api.soto({tab: this.props.cursor});
+  }
+  // capture and transmit most characters
+  else if (ignoredKeys.indexOf(e.key) === -1) {
+    store.doEdit({ ins: { cha: e.key, at: this.props.cursor } });
+    store.setState({ cursor: this.props.cursor + 1 });
+  }
+}
+
+render() {
+  return (
+    <div className="flex flex-row" 
+    style={{ flexGrow: 1 }}>
+    ~{this.props.ship}:dojo
+    <span id="prompt">
+    {this.props.prompt}
+    </span>
+    <input 
+    autoCorrect="false" 
+    autoFocus={true}
+    className="mono ml1 flex-auto dib w-100"
+    cursor={this.props.cursor}
+    onKeyDown={this.keyPress}
+    onPaste={e => {e.preventDefault()}}
+    ref={this.inputRef}
+    defaultValue={this.props.input}
+    />
+    </div>
+    )
+  }
 }
 
 export default Input

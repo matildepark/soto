f35cd95d26118edb60b80fb06e1dbdf1e72859b7
Matilde Park <matilde@tlon.io>
2019-11-14 11:37:27 -0500

    some solejs legibility

--- src/js/components/lib/sole.js
+++ src/js/components/lib/sole.js
@@ -11,8 +11,14 @@ export class Share {
     if (leg == null) { leg = []; }
     this.leg = leg;
   }
-  //
-  abet() { return {buf:this.buf, leg:this.leg.slice(), ven:this.ven.slice()}; }
+
+  abet() { 
+    return { buf:this.buf, 
+             leg:this.leg.slice(), 
+             ven:this.ven.slice() 
+           }; 
+    }
+
   apply(ted){
     switch (false) {
       case 'nop' !== ted: return;
@@ -21,13 +27,13 @@ export class Share {
         case 'set': return this.buf = ted.set;
         case 'del': return this.buf = this.buf.slice(0,ted.del) + this.buf.slice(ted.del + 1);
         case 'ins':
-          var {at,cha} = ted.ins;
+          var { at, cha } = ted.ins;
           return this.buf = this.buf.slice(0,at) + cha + this.buf.slice(at);
         default: throw `%sole-edit -lost.${str(ted)}`;
       }
     }
   }
-  //
+
   transmute(sin,dex){
     switch (false) {
       case (sin !== 'nop') && (dex !== 'nop'): return dex;
@@ -39,14 +45,14 @@ export class Share {
         case 'set': return 'nop';
         case 'del':
           if (sin.del === dex.del) { return 'nop'; }
-          dex = { ...dex }  // clone
+          dex = { ...dex }
           switch (Object.keys(dex)[0]) {
             case 'del': if (sin.del < dex.del) {    dex.del--; } break;
             case 'ins': if (sin.del < dex.ins.at) { dex.ins.at--; } break;
           }
           return dex;
         case 'ins':
-          dex = { ...dex };  // clone
+          dex = { ...dex };
           var {at,cha} = sin.ins;
           switch (Object.keys(dex)[0]) {
             case 'del': if (at < dex.del) { dex.del++; } break;
@@ -60,13 +66,13 @@ export class Share {
       }
     }
   }
-  //
+
   commit(ted){
     this.ven[0]++;
     this.leg.push(ted);
     return this.apply(ted);
   }
-  //
+
   inverse(ted){
     switch (false) {
       case 'nop' !== ted: return ted;
@@ -80,32 +86,39 @@ export class Share {
       }
     }
   }
-  //
+
   receive({ler,ted}){
     if (!(ler[1] === this.ven[1])) { 
       throw `-out-of-sync.[${str(ler)} ${str(this.ven)}]`;
     }
     this.leg = this.leg.slice((this.leg.length + ler[0]) - this.ven[0]); 
     const dat = this.transmute(this.leg, ted);
-    this.ven[1]++; this.apply(dat); return dat;
+    this.ven[1]++; 
+    this.apply(dat); 
+    return dat;
   }
-  //
-  remit() { throw 'stub'; }
+
+  remit() { 
+    throw 'stub'; 
+  }
+
   transmit(ted){
     const act = {ted, ler:[this.ven[1], this.ven[0]]};
     this.commit(ted);
     return act;
   }
-  //
+
   transceive({ler,ted}){
     const old = new Share(this.buf);
     const dat = this.receive({ler, ted});
     return old.inverse(dat);
   }
-  //
+
   transpose(ted,pos){
-    if (pos === undefined) { return this.transpose(this.leg, ted);
-    } else { let left;
+    if (pos === undefined) { 
+      return this.transpose(this.leg, ted);
+    } else { 
+      let left;
     return ((left = (this.transmute(ted, {ins: {at: pos}})).ins) != null ? left : {at:0}).at; }
   }
 };

2c13b5e797b61cfc843a15923ff2289c7686f73e
Matilde Park <matilde@park.computer>
2019-11-14 00:06:39 -0500

    remove faux eval

--- tile/tile.js
+++ tile/tile.js
@@ -9,12 +9,7 @@ export default class sotoTile extends Component {
     return (
       <div className="w-100 h-100 relative bg-black ba b--gray" style={{ }}>
         <a className="pa2 no-underline b label-regular absolute" href="/~soto"
-        style={{ right: 8, top: 4, color: "#777", padding: 0 }}>Dojo -></a>
-        <section class="eval mono gray flex pt4 pl1 pr1"
-        style={{fontFamily: `"Source Code Pro", monospace`, lineHeight: "1.4"}}>~{window.ship}:eval>
-        <input autoFocus class="dib flex-auto bg-black gray bn ml1"
-        style={{fontFamily: `"Source Code Pro", monospace`}}></input>
-        </section>
+        style={{ left: 8, top: 4, color: "#777", padding: 0 }}>Dojo</a>
       </div>
     );
   }
